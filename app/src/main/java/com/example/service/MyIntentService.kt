a411fd9d6a072a7a6c6390faac29068690fbe0b6
danilfs009@yandex.ru <62214743+danilfs@users.noreply.github.com>
2022-08-01 09:41:55 +0300

    WorkerThread

--- app/src/main/java/com/example/service/MyIntentService.kt
+++ app/src/main/java/com/example/service/MyIntentService.kt
@@ -5,10 +5,11 @@ import android.content.Intent
 import android.os.Handler
 import android.os.HandlerThread
 import android.os.IBinder
+import androidx.annotation.WorkerThread
 
 abstract class MyIntentService(name: String): Service() {
 
-    private val serviceThread = HandlerThread(name)
+    private val serviceThread = WorkerThread(name)
 
     override fun onBind(intent: Intent?): IBinder? {
         return null
@@ -21,11 +22,11 @@ abstract class MyIntentService(name: String): Service() {
 
     override fun onDestroy() {
         super.onDestroy()
-        serviceThread.looper.quit()
+        serviceThread.stop()
     }
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
-        Handler(serviceThread.looper).post {
+        serviceThread.post {
             onHandleIntent(intent)
             stopSelf()
         }
